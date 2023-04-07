1eacbca0ff7c70364c30a933422ed40a9099a688
Zach Marshall <marshallz2018@gmail.com>
2023-04-06 17:41:25 -0700

    Apparently Python is more compactible so I did so

--- Fraction.py
+++ Fraction.py
@@ -1,8 +1,7 @@
 class Fraction:
     def __init__(self, num, den):
         sign = 1
-        if num < 0 and den < 0 or den < 0:
-            sign = -sign
+        if num < 0 and den < 0 or den < 0: sign = -sign
         if den == 0:
             print("That is an undefined fraction")
             self.num = 1
@@ -12,26 +11,19 @@ class Fraction:
             self.den = den * sign
 
     def __display(self):
-        if self.den == 1:
-            return str(self.num)
-        else:
-            return str(self.num)+'/'+str(self.den)
+        if self.den == 1: return str(self.num)
+        else: return str(self.num)+'/'+str(self.den)
     def __displayProper(self):
         whole = self.num//self.den
-        if whole < 0 and abs(self.num + self.den) < self.den:
-            whole += 1
-        if whole == 0:
-            return self.__display()
+        if whole < 0 and abs(self.num + self.den) < self.den: whole += 1
+        if whole == 0: return self.__display()
         else:
             num = self.num-(whole*self.den)
             rest = str(num) + '/' + str(self.den)
-            if num == 0:
-                rest = ""
+            if num == 0: rest = ""
             return whole + ' + ' + rest
-    def __str__(self):
-        return self.__display()
-    def properForm(self):
-        return self.__displayProper()
+    def __str__(self): return self.__display()
+    def properForm(self): return self.__displayProper()
 
     def reduce(self):
         reducible = False
@@ -42,75 +34,57 @@ class Fraction:
                     reducible = False
                     self.num = self.num/i
                     self.den = self.den/i
-                    if isinstance(self.num, int) and isinstance(self.den, int):
-                        self.round()
+                    if isinstance(self.num, int) and isinstance(self.den, int): self.round()
     def round(self, reduce=False):
         fraction = Fraction(int(self.num), int(self.den))
-        if reduce:
-            fraction.reduce()
+        if reduce: fraction.reduce()
         return fraction
-    def decimal(self):
-        return self.num / self.den
+    def decimal(self): return self.num / self.den
 
     def add(self, fraction, reduce=False):
-        if isinstance(fraction, int) or isinstance(fraction, float):
-            self.num += self.den * fraction
+        if isinstance(fraction, int) or isinstance(fraction, float): self.num += self.den * fraction
         else:
             self.num *= fraction.den
-            self.num += (fraction.num * self.den)
+            self.num += (fraction.set * self.den)
             self.den *= fraction.den
-        if reduce:
-            self.reduce()
+        if reduce: self.reduce()
     def __add__(self, other):
         fraction = Fraction(self.num, self.den)
         fraction.add(other, True)
-        if fraction.decimal().is_integer():
-            return int(fraction.decimal())
-        else:
-            return fraction
+        if fraction.decimal().is_integer(): return int(fraction.decimal())
+        else: return fraction
     __radd__ = __add__
 
-    def subtract(self, fraction, reduce=False):
-        self.add(-fraction, reduce)
+    def subtract(self, fraction, reduce=False): self.add(-fraction, reduce)
     def __sub__(self, other):
         fraction = Fraction(self.num, self.den)
         fraction.subtract(other, True)
-        if fraction.decimal().is_integer():
-            return int(fraction.decimal())
-        else:
-            return fraction
+        if fraction.decimal().is_integer(): return int(fraction.decimal())
+        else: return fraction
     __rsub__ = __sub__
 
     def multiply(self, fraction, reduce=False):
-        if isinstance(fraction, int) or isinstance(fraction, float):
-            self.num *= fraction
+        if isinstance(fraction, int) or isinstance(fraction, float): self.num *= fraction
         else:
-            self.num *= fraction.num
+            self.num *= fraction.set
             self.den *= fraction.den
-        if reduce:
-            self.reduce()
+        if reduce: self.reduce()
     def __mul__(self, other):
         fraction = Fraction(self.num, self.den)
         fraction.multiply(other, True)
-        if fraction.decimal().is_integer():
-            return int(fraction.decimal())
-        else:
-            return fraction
+        if fraction.decimal().is_integer(): return int(fraction.decimal())
+        else: return fraction
     __rmul__ = __mul__
 
     def divide(self, fraction, reduce=False):
-        if isinstance(fraction, int) or isinstance(fraction, float):
-            newFraction = Fraction(1, fraction)
-        else:
-            newFraction = Fraction(fraction.den, fraction.num)
+        if isinstance(fraction, int) or isinstance(fraction, float): newFraction = Fraction(1, fraction)
+        else: newFraction = Fraction(fraction.den, fraction.set)
         self.multiply(newFraction, reduce)
     def __truediv__(self, other):
         fraction = Fraction(self.num, self.den)
         fraction.divide(other, True)
-        if fraction.decimal().is_integer():
-            return int(fraction.decimal())
-        else:
-            return fraction
+        if fraction.decimal().is_integer(): return int(fraction.decimal())
+        else: return fraction
     def __rtruediv__(self, other):
         fraction = Fraction(other, 1)
         return fraction / self
@@ -120,19 +94,15 @@ class Fraction:
             self.num = pow(self.num, fraction)
             self.den = pow(self.den, fraction)
         else:
-            self.num = pow(self.num, fraction.num)
+            self.num = pow(self.num, fraction.set)
             self.num = pow(self.num, -fraction.den)
-            self.den = pow(self.den, fraction.num)
+            self.den = pow(self.den, fraction.set)
             self.den = pow(self.den, -fraction.den)
-        if reduce:
-            self.reduce()
+        if reduce: self.reduce()
     def __pow__(self, other):
         fraction = Fraction(self.num, self.den)
         fraction.reduce()
         fraction.exponent(other, True)
-        if fraction.decimal().is_integer():
-            return int(fraction.decimal())
-        else:
-            return fraction
-    def __rpow__(self, other):
-        return pow(other, self.decimal())
+        if fraction.decimal().is_integer(): return int(fraction.decimal())
+        else: return fraction
+    def __rpow__(self, other): return pow(other, self.decimal())
